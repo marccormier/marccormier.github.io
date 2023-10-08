8487fb98fe92f49260ffd38c53d115b4748c004c
mccormier <cormiermac.mc85@gmail.com>
2023-10-08 08:02:03 -0400

    update algorithm and structure

--- content/scripts/nav.js
+++
@@ -1,7 +0,0 @@
-// nav.html
-// change index.html innerHTML
-var navigation=document.getElementById('contentNavigation');
-function navigate() {
-	// get the onclick event object window location
-	// let navigation.innerHTML="";
-}
