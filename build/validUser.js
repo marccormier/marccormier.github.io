ba38ada28b5e577c314c96d3fb26cfad0bc1fdca
mccormier <cormiermac.mc85@gmail.com>
2023-10-27 09:34:45 -0400

    quick update

--- build/validUser.js
+++ build/validUser.js
@@ -4,6 +4,8 @@ x=document.getElementById("vUname").value;
 w=x.slice(0, 1).toUpperCase();
 v=x.slice(1).toLowerCase();
 y=document.getElementById("vUpw").value;
+z=document.getElementById("dom");
+/*
 // localStorage.getItem('key') ? JSON.parse(localStorage.getItem('key')) : '';
 //
 	const alphaNumRegEx = /^[-0-9a-zA-Z]+$/;
@@ -11,8 +13,12 @@ y=document.getElementById("vUpw").value;
 		if (key.match(alphaNumRegEx)) {
 			console.log(key); 
 //
-			document.body.innerHTML="<center><h1>Bonjour, "+w+v+"</h1></center><hr>";
+*/
+			//document.body.innerHTML="<center><h1>Bonjour, "+w+v+"</h1></center><hr>";
+			z.innerHTML="Bonjour, "+w+v+"<hr>";
+/*
 		} 
 	}); 
-connection.close();
+*/
+			connection.close();
 }
