9acbf58b33d4a070b3394903d957f35a4617b90a
mccormier <cormiermac.mc85@gmail.com>
2023-11-07 00:00:02 -0500

    quick update

--- content/js/eventTarget.js
+++
@@ -1,6 +0,0 @@
-/* Event Target tagName*/
-//body onload 
-function eventTarget(event) { 
-  const element=event.target;
-  document.getElementById("domB").innerHTML = "Triggered by " + element.tagName;
-}
---
+++ js/script.js
@@ -0,0 +1,4 @@
+bScript=document.createElement("script");
+bScript.type="text/javascript";
+bScript.innerHTML=needles;
+document.body.append(bScript);
