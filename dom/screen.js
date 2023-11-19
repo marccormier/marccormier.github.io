93c5ea267c59ab50794e080b408b5e62e8c5b09f
mccormier <cormiermac.mc85@gmail.com>
2023-11-19 03:58:12 -0500

    quick update js html dom

--- dom/screen.js
+++
@@ -1,6 +0,0 @@
-console.info(screen.availHeight);
-console.info(screen.availWidth);
-console.info(screen.colorDepth);
-console.info(screen.height);
-console.info(screen.pixelDepth);
-console.info(screen.width);
