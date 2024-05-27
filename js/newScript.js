6a02c2f10b84d8cf5b57a986b14ada1e6ba5384a
mccormier <cormiermac.mc85@gmail.com>
2024-05-26 21:16:41 -0400

    quick update

--- js/newScript.js
+++ js/newScript.js
@@ -9,7 +9,7 @@ document.head.append(bScript);
 //
 function O(i) { return typeof i == 'object' ? i : document.getElementById(i) }
 content=O('content');
-let accueil=' <center> <img src="img/maintenance.jpg"> <h3 style="text-decoration:underline;">Last Updated Scripts</h3> <ul> <li><a href="skydiveLoggerApp">Skydive Logger APP Data To HTML Table</a></li> <li><a href="storage.htm">Wweb Storage (JAVA webEngine lOCAL data Storage)</a></li> <li><a href="RGBAcolors.htm">RGBA Colors</a></li> <li><p>navigation</p></li> </ul> </center>';
+let accueil='<center> <img src="img/maintenance.jpg"> </center>';
 function home() {O('content').innerHTML=accueil; }
 function about() {O('content').innerHTML='<h2>Information Personnel</h2><hr><p>Mon nom est Marc et j&#39ai 38 ans.  Je vie dans une grande ville de plus ou moins 423 000 habitants, située au Québec, Canada. <br />Magnifique region, que j&#39affectionne tout particulierement. J&#39estime que l&#39age, la couleur, la religion, l&#39apparence physique, tout ce qui nous distingue (et nous sépare souvent aussi) au quotidien n&#39a pas lieu d&#39être sur le net. <br/>Je documente tout ce que j&#39apprends et aide les gens à commencer dans le codage. <br />Mon site n&#39a pas de publicités, de sponsors ou de &#39bullshit&#39. <br /><br />Je n&#39aime pas parler de moi, mais quand je visite un site et que je vois une rubrique consacree au webmaster, je clique sur le lien, et donc Merci de sacrifie de votre precieux temps afin de visite mon site, je souhaite exprimer ma gratitude a mes visiteurs.<br /><br />J&#39espere que votre consultation répondra a vos attentes. <br /><br />Je suis très intéressé par le codage, après un peu plus de 20 ans de presence sur le net, j&#39ai continuer à apprendre le codage avec <a href="https://www.sololearn.com/">sololearn</a>. Je me suis ensuite inscrit au formations informatique à l&#39<a href="https://www.formation-ifp.com/">institut de formation professionnel</a> (Entretien &#38; Réparation D&#39ordinateur, Spécialiste Internet et Programmation JAVA).<br />Toussent terminer avec une note finale de plus de 80% ainsi j&#39ai aussi choisis de creer un site sans pour autant tout vous devoiler, car le net reste malgre tout un monde virtuel, et j&#39aime garder une petite part du mystere sur ma personalitée.<br /><br />Si vous appréciez mon contenu, pensez à soutenir ce que je fais.</p>'; }
 //
@@ -97,14 +97,14 @@ content.appendChild(jQuerySnip);
 }
 //
 function skills() {O('content').innerHTML='<h2>Mes Compétences</h2><hr><center><ul><li>HTMl</li><li>CSS</li><li>JavaScript</li><li>PHP</li><li>JAVA</li></ul></center>'; }
-const htmlCodes='<h3>HTML</h3> <ul>Color Picker<br/> <input type="color" id="colorpicker" value="#000000" style="margin-left:0px;" /> <li><a class="domAlink" onclick="passwordGenerator(random_number)">Générateur de mot de passe</a></li> <li><a class="domAlink" onclick="winNav()">Navigator Properties</a></li> <li><a class="domAlink" onclick="cssSnips()">CSS html DOM Snippets</a></li> <li><a class="domAlink" onclick="domStyles()">JS html DOM Styles</a></li> <li><a class="domAlink" onclick="htmlDOMnode()">JS HTML DOM Interface</a></li> <li><a onclick="peridotColorPattern()">Peridot color pattern</a></li> <li><a class="domAlink" onclick="htmlCompiler()">Compilateur HTML</a></li> <li><a class="domAlink" onclick="binCompiler()">Compilateur Binaire</a></li> <li><a class="subTcodes">Window Object</a></li> <li><a>HTML Events</a></li> <li><a href="storage.htm"><i>web Storage</i></a></li> <li><a href="RGBAcolors.htm"><i>RGBA Colors</i></a></li> </ul> <br/>';
-const cssCodes='<h3>CSS</h3><ul><li><a>CSS Needles Library</a></li></ul><br/>';
-const jsCodes='<h3>JavaScript</h3><ul><li><a>AJAX</a></li><li><a>Time</a></li><li><a>Gestion d&#39opérations</a></li><li><a>Générateur de Table HTML</a></li><li><a>OSC</a></li><li><a>needles</a></li></ul> <br/>';
-const jQueryCodes='<h3>jQuery</h3> <ul> <li><a>Gestion des mouvements et des boutons de la souris</a></li> <li><a class="domAlink" onclick="jQuerySnips()">jQuery Snippets</a></li> </ul> <br/>';
-const phpCodes='<h3>PHP</h3><ul><li><a>Gestion de fichiers</a></li><li><a>Appels Système</a></li><li><a>Gestion de Formulaire</a></li><li><a>Gestion de cookies</a></li><li><a>Gestion d&#39utilisateurs</a></li><li><a>Gestion de Session</a></li><li><a>Gestion d&#39erreurs</a></li></ul><br/>';
-const javaCodes='<h3>JAVA</h3><ul><li><a>Coffee Machine</a></li><li><a>Gestion de fichiers</a></li><li><a>Gestion de bulletin de notes</a></li><li><a>Gestion de comptes bancaire</a></li><li><a>Sapin en Swing</a></li><li><a>Local Storage JSON Array Object to JAVA WebEngine html DOM Core</a></li></ul><br/>';
-const shellScriptCodes='<h3>Shell Scripting Language</h3><ul><li><a>file Sorting</a></li> <li><a>List Files In Folder</a></li> <li><a>Windows error Fixer</a></li> <li><a>Clone File Checker</a></li> <li><a>clear Cache</a></li> </ul><br/>';
-const mslCodes='<h3>mSL</h3><ul><li><a>MCBrady</a></li></ul>';
+const htmlCodes='<h3>HTML (.htm - .html)</h3> <ul>Color Picker<br/> <input type="color" id="colorpicker" value="#000000" style="margin-left:0px;" /> <li><a class="domAlink" onclick="passwordGenerator(random_number)">Générateur de mot de passe</a></li> <li><a class="domAlink" onclick="winNav()">Navigator Properties</a></li> <li><a class="domAlink" onclick="cssSnips()">CSS html DOM Snippets</a></li> <li><a class="domAlink" onclick="domStyles()">JS html DOM Styles</a></li> <li><a class="domAlink" onclick="htmlDOMnode()">JS HTML DOM Interface</a></li> <li><a onclick="peridotColorPattern()">Peridot color pattern</a></li> <li><a class="domAlink" onclick="htmlCompiler()">Compilateur HTML</a></li> <li><a class="domAlink" onclick="binCompiler()">Compilateur Binaire</a></li> <li><a class="subTcodes">Window Object</a></li> <li><a>HTML Events</a></li> <li><a href="storage.htm"><i>web Storage</i></a></li> <li><a href="RGBAcolors.htm"><i>RGBA Colors</i></a></li> <li><a href="skydiveLoggerApp">Skydive Logger APP Data To HTML Table</a></li> </ul> <br/>';
+const cssCodes='<h3>CSS (.css)</h3><ul><li><a>CSS Needles Library</a></li></ul><br/>';
+const jsCodes='<h3>JavaScript (.js)</h3><ul><li><a>AJAX</a></li><li><a>Time</a></li><li><a>Gestion d&#39opérations</a></li><li><a>Générateur de Table HTML</a></li><li><a>OSC</a></li><li><a>needles</a></li></ul> <br/>';
+const jQueryCodes='<h3>jQuery (.js)</h3> <ul> <li><a>Gestion des mouvements et des boutons de la souris</a></li> <li><a class="domAlink" onclick="jQuerySnips()">jQuery Snippets</a></li> </ul> <br/>';
+const phpCodes='<h3>PHP (.php)</h3><ul><li><a>Gestion de fichiers</a></li><li><a>Appels Système</a></li><li><a>Gestion de Formulaire</a></li><li><a>Gestion de cookies</a></li><li><a>Gestion d&#39utilisateurs</a></li><li><a>Gestion de Session</a></li><li><a>Gestion d&#39erreurs</a></li></ul><br/>';
+const javaCodes= ' <h3>JAVA (.java)</h3> <ul> <li><a>Coffee Machine</a></li> <li><a>Gestion de fichiers</a></li> <li><a>Gestion de bulletin de notes</a></li> <li><a>Gestion de comptes bancaire</a></li><li><a>Sapin en Swing</a></li></ul><br/> ';
+const shellScriptCodes='<h3>Shell Scripting Language (.bat - .cmd - .exe)</h3><ul><li><a>file Sorting</a></li> <li><a>List Files In Folder</a></li> <li><a>Windows error Fixer</a></li> <li><a>Clone File Checker</a></li> <li><a>clear Cache</a></li> </ul><br/>';
+const mslCodes='<h3>mSL (.ini)</h3><ul><li><a>MCBrady</a></li></ul>';
 function codes() {O('content').innerHTML='<h2>Codes</h2><hr><p>Jusqu&#39à présent, j&#39ai écrit quelques codes :</p>'+ htmlCodes+cssCodes+jsCodes+jQueryCodes+phpCodes+javaCodes+shellScriptCodes+mslCodes; }
 //
 function winNav(){
@@ -138,7 +138,7 @@ function winNav(){
     O('content').innerHTML=caracteristiquesNavigateur;
 }
 //
-function login() {
+function members() {
   O('content').innerHTML="<h2>Tableau de bord</h2>";
 let storedText;
 fetch('https://ipinfo.io/json?callback') .then(function(response) {
@@ -149,6 +149,19 @@ fetch('https://ipinfo.io/json?callback') .then(function(response) {
     O('content').innerHTML+=dt;
   });
 });
+setTimeout(function(){
+  let text;
+  let uFname=document.getElementsByTagName('h2')[0];
+  let uName=sessionStorage.getItem('uFname');
+//if (uName!=null || uName!=""){uFname.innerHTML=uName;}
+//if (sessionStorage.uFname){uFname.innerHTML=sessionStorage.uFname;}
+  let person = prompt("S'il vous plaît entrez votre nom:", "");
+  if (person == null || person == "") {text = "Cancelled prompt.";}
+   else {text = "Bonjour "+person+"!";}
+  uFname.innerHTML = text;
+  sessionStorage.setItem('uFname', text);
+}, 1000); //Time before execution
+//
 bientot();
 }
 //
@@ -160,97 +173,8 @@ function bientot() {
   return true;
 }
 //
+//function services() {window.open('Service')};
 function services() {O('content').innerHTML="<h2>Services</h2>"+ "ENTRETIEN & RÉPARATION D'ORDINATEUR<br/>"+ "Diagnostique des problèmes matériel ou logiciel, Récupération de mots de passe, Récupération et sauvegarde de données, Nettoyage à l'air de vos équipements électroniques et ordinateurs, Remplacement de disque dur, Installation et configuration d’imprimante, Élimination de virus, nettoyage d’ordinateur Formatage, installation de Windows<br/><br/>"+ "<center><img id='maintenance' style='max-width:50vw;height:auto;' src='img/maintenance.jpg'><br/>"+ "MAINTENANCE & DÉPANNAGE RÉSEAUX<br/>"+ "Configuration de réseau sans-fil, Câblages, Codage des données, topologie, protocoles, adressage<br/><br/>"+ "CONCEPTION GRAPHIQUE & WEBDESIGN<br/>"+ "Logos, templates Web, plaquettes publicitaires, cartes de visite, newsletters...<br/><br/>"+ "INTÉGRATION (X)HTML / CSS<br/>"+ "Codage à la main respectueux des standards du Web<br/><br/>"+ "GESTION DE PROJETS WEB<br/>"+ "Site vitrine, corporate, évènementiel, e-commerce, intranet, application mobile.<br/><br/>"+ "APPLICATIONS SPÉCIFIQUES ET INTERFACE D'ADMINISTRATION<br/><br/>"+ "DYNAMISE DES PAGES PAR JAVASCRIPT<br/><br/>"+ "VALIDATION W3C, WAI & RÉFÉRENCEMENT NATUREL SEO<br/>"+ "Accessibilité & ergonomie des pages web<br/><br/>"+ "CONCEPTION MULTI-PLATEFORMES<br/>"+ "Compatible tous supports, tablette & application mobile<br/><br/>"+ "FORMATION PERSONALISÉE<br/>"+ "Débutant - Intermédiaire - Expert<br/><br/>"+ "Le service informatique à domicile est disponible pour les particuliers, les travailleurs autonomes et les petites entreprises.<br/>"+ "Service à domicile sur rendez-vous.<br/><br>"+ "<input style='margin:7px;padding:3px;font-weight:bold;' type='button' onclick='tarif()' value='Tarif' />"+ "<input style='margin:7px;padding:3px;font-weight:bold;' type='button' onclick='zone()' value='Zone de service' /><br/><br/>"; }
 function tarif() {O('content').innerHTML="<h2>Tarif</h2>"+ "Estimation à partir de 20$ * <br/><br/>"+ "Tarif à partir de 45$ de l'heure <br/><br/>"+ "*** Service commercial: a partir de 75$ de l'heure *** <br/><br/>"+ "Service d'assistance en ligne pour 15$ jusqu'à 30 minutes!<br/><br/>"+ "Service rapide à partir de $85<br/>"+ "** prix minimum pour un déplacement: 1 heure selon tarif.<br/>"+ "* Minimum 1 heure  *<br/><br/>"+ "* peut varier en fonction des certifications et/ou compétences du technicien ou administrateur reseaux.<br/><br/>"+ "** prix sujet à changement sans préavis. pour plus de détails visitez notre site web régulièrement.<br/><br/>"+ "<input style='margin:7px;padding:3px;font-weight:bold;' type='button' onclick='zone()' value='Zone de service' /><br/><br/>"; }
 function zone() {O('content').innerHTML="<h2>Zone de service</h2>"+ "<p style='text-decoration:line-through;'>Rive-Nord</p>"+ "Laval<br/>"+ "Montréal<br/>"+ "<p style='text-decoration:line-through;'>Rive-Sud</p>"+ "<input style='margin:7px;padding:3px;font-weight:bold;' type='button' onclick='tarif()' value='Tarif' />"; }
 //
-setInterval(clock, 1);
-function clock() {
-const d = new Date();
-let time = d.getTime();
-let sec = d.getSeconds();
-let min = d.getMinutes();
-let millisec = d.getMilliseconds();
-let hour = d.getHours();
-let utc = d.toUTCString();
-let fYear = d.getFullYear();
-let date = d.getDate();
-let day;
-
-/*
-const weekday = ["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"];
-const d = new Date();
-let day = weekday[d.getDay()];
-*/
-
-switch (d.getDay()) {
-  case 0:
-  day = "Dimanche";
-  break;
-  case 1:
-  day = "Lundi";
-  break;
-  case 2:
-  day = "Mardi";
-  break;
-  case 3:
-  day = "Mercredi";
-  break;
-  case 4:
-  day = "Jeudi";
-  break;
-  case 5:
-  day = "Vendredi";
-  break;
-  case  6:
-  day = "Samedi";
-}
-
-let month;
-switch (d.getMonth()) {
-  case 0:
-  month = "Janvier";
-  break;
-  case 1:
-  month = "Février";
-  break;
-  case 2:
-  month = "Mars";
-  break;
-  case 3:
-  month = "Avril";
-  break;
-  case 4:
-  month = "Mai";
-  break;
-  case 5:
-  month = "Juin";
-  break;
-  case  6:
-  month = "Juillet";
-  break;
-  case 7:
-  month = "Août";
-  break;
-  case  8:
-  month = "Septembre";
-  break;
-  case  9:
-  month = "Octobre";
-  break;
-  case  10:
-  month = "Novembre";
-  break;
-  case  11:
-  month = "Décembre";
-}
-hour = hour < 10 ? "0" + hour : hour;
-min = min < 10 ? "0" + min : min;
-sec = sec < 10 ? "0" + sec : sec;
-month = month < 10 ? "0" + month : month;
-day = day < 10 ? "0" + day : day;
-let res=day+", "+date+"&nbsp"+month+"&nbsp"+fYear+"<br/>"+hour+":"+min+":"+sec+"."+millisec;
-let utcTime=utc;
-document.getElementById("time").innerHTML=res;
-document.getElementById("uTime").innerHTML=utcTime;
-}
