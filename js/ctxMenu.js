df6d8e4c3aa8aeff5ce2ad83036b66cbae4f7bda
marccormier <cormiermac.mc85@gmail.com>
2024-11-07 06:04:02 -0500

    ajustement du language ..

--- js/ctxMenu.js
+++ js/ctxMenu.js
@@ -13,12 +13,13 @@ oncontextmenu = (e) => {
   webStorage="<p onclick='window.open(`storage.htm`, `_self`)'>Web Storage</p>";
   mcbrady="<p onclick='window.open(`mcbrady`, `_self`)'>#MCbrady IRC Channel</p>";
   esoGuild="<p onclick='window.open(`eso`, `_self`)'>ESO Guild</p>";
+  budgetTracker="<p onclick='window.open(`Budget`, `_self`)'>Budget Tracker</p>";
   olderVersion="<p onclick='window.open(`olderVersion/wix-2021-12-30-13h13m.htm`, `_self`)'>Ancienne Version.</p>";
   //extLinks="<p onclick='window.open(`extLinks.htm`, `_self`)'>Liens Externe</p>";
   //scopes="<p onclick='window.open(`scope.htm`, `_self`)'>Code Scopes</p>";
   //
   //codes="<p onclick='codes()'>Codes</p>";
   //alert="<p onclick='alert(`you click option alert`)'>alert</p>";
-  menu.innerHTML = mTitle+opt+webStorage+mcbrady+esoGuild;
+  menu.innerHTML = mTitle+opt+webStorage+mcbrady+esoGuild+budgetTracker;
   document.body.appendChild(menu);
 }
