d8f805ff37ae208fe4e94fdaca58dae013fac730
marccormier <cormiermac.mc85@gmail.com>
2025-08-15 12:35:30 -0400

    quick update

---
+++ PilotTools/mocorgo-master/README.md
@@ -0,0 +1,5 @@
+# mocorgo
+Building a physics engine\
+Youtube - https://www.youtube.com/channel/UCORSpAcEfLWPRnbD2AAizSg  
+
+Newest version is always in the Game Template folder
--- js/ctxMenu.js
+++ js/ctxMenu.js
@@ -8,7 +8,8 @@ oncontextmenu = (e) => {
   menu.id = "ctxmenu"
   menu.style = `top:${e.pageY-10}px;left:${e.pageX-40}px`;
   menu.onmouseleave = () => ctxmenu.outerHTML = ''
-  mTitle="Menu<br><hr>";
+  mTitle="<center><b>Menu</b></center><br><hr>";
+  pilotCodeTool="<p onclick='window.open(`PilotTools`, `_self`)'>ParaGliding Pilot Coding Tool's</p>";
   opt="<p onclick='alert(`Option...`)'>Alert('Option...')</p>";
   webStorage="<p onclick='window.open(`storage.htm`, `_self`)'>Web Storage</p>";
   mcbrady="<p onclick='window.open(`mcbrady`, `_self`)'>#MCbrady IRC Channel</p>";
@@ -21,6 +22,6 @@ oncontextmenu = (e) => {
   //
   //codes="<p onclick='codes()'>Codes</p>";
   //alert="<p onclick='alert(`you click option alert`)'>alert</p>";
-  menu.innerHTML = mTitle+opt+pCustomDesign+mcbrady+esoGuild+budgetTracker;
+  menu.innerHTML = mTitle+pilotCodeTool+opt+pCustomDesign+mcbrady+esoGuild+budgetTracker;
   document.body.appendChild(menu);
 }
